759466872ec92df323b0c5a4b02a9f24c8be884f
Chaka <hongoke999@yahoo.com>
2020-04-28 17:57:40 +0300

    Variables - var, let & const

--- section2/app.js
+++ section2/app.js
@@ -1,21 +1,52 @@
-console.log("Hello, World!");
-console.log(123);
-console.log(true);
-var greeting = 'Hello!';
+// var, let, const
+
+/* var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name); */
+
+// Init var
+
+/* var greeting;
 console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
-console.error('This is some error!');
-console.warn('This is a warning!');
-console.time('Hello');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-console.timeEnd('Hello');
+greeting = 'Hello';
+console.log(greeting); */
+
+// letters, numbers, _, $
+// Cannot start with number
+
+// Multi word vars
+
+/* var firstName = 'John'; // Camel case - Javascript convention
+var first_name = 'Sara'; // Underscore - PHP convention
+var FirstName = 'Tom'; // Pascal case - when creating a constructors or classes with ES6 */
+
+// LET
+
+/* let name; 
+name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+ */
+
+// CONST
+const name = 'John Doe';
+console.log(name);
+// Cannot reassign
+//name = 'Steve Smith';
+// Have to assign a value
+//const greeting;
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.name = 'Sara';
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
