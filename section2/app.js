bca30e3d1b3fdc50c8dee68c61f39b804bbb8b9a
Chaka <hongoke999@yahoo.com>
2020-04-29 21:21:58 +0300

    Numbers & The Math Object

--- section2/app.js
+++ section2/app.js
@@ -1,21 +1,27 @@
-console.log("Hello, World!");
-console.log(123);
-console.log(true);
-var greeting = 'Hello!';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
-console.error('This is some error!');
-console.warn('This is a warning!');
-console.time('Hello');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-console.timeEnd('Hello');
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
