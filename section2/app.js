df68458a0534d812aff1fc1fcb3d9a1d0036a9ca
Chaka <hongoke999@yahoo.com>
2020-04-29 13:38:48 +0300

    Type Conversion

--- section2/app.js
+++ section2/app.js
@@ -1,21 +1,42 @@
-console.log("Hello, World!");
-console.log(123);
-console.log(true);
-var greeting = 'Hello!';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
-console.error('This is some error!');
-console.warn('This is a warning!');
-console.time('Hello');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-    console.log("Hello, World!");
-    console.log('Hello, World!');
-console.timeEnd('Hello');
+let val;
+
+// Number to String
+val = String(555);
+val = String(4+4);
+
+// Bool to String
+val = String(true);
+
+// Date to String
+val = String(new Date());
+
+// Array to String
+val = String([1, 2, 3, 4]);
+
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to Number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3, 4]);
+
+val = parseInt('100');
+val = parseFloat('100.31');
+
+// Output
+/* console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed(2)); */
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
