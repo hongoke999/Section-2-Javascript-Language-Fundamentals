6d38560b8a5a4ccb05de92749be289c12b6106cc
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-03 12:51:22 +0300

    A Look At The Window Object

--- section2/app.js
+++ section2/app.js
@@ -1,78 +1,57 @@
-// FOR LOOP
-
-/* for(let i = 0; i < 10; i++) {
-    // console.log('Number ' + i);
-
-    if(i === 2) {
-        console.log('2 is my favourite number');
-        continue;
-    }
-
-    if(i === 5) {
-        console.log('Stop the loop');
-        break;
-    }
-
-    console.log('Number ' + i);
-} */
-
-// WHILE LOOP
-
-/* let i = 0;
-
-while(i < 10) {
-    console.log('Number ' + i);
-    i++;
-} */
-
-// DO WHILE 
-
-/* let i = 0;
-
-do {
-    console.log('Number ' + i);
-    i++;
-} while(i < 10); */
-
-// LOOP THROUGH ARRAY
-const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
-/* 
-for(let i = 0; i < cars.length; i++) {
-    console.log(cars[i]);
-} */
-
-// FOREACH
-/* cars.forEach(function(car){
-    console.log(car);
-}); 
-
-cars.forEach(function (car, index, array) {
-  console.log(`${index} : ${car}`);
-  console.log(array);
-}); */
+// WINDOW METHODS / OBJECTS /PROPERTIES
 
 
-// MAP
-/* const users = [
-    {id: 1, name: 'John'},
-    {id: 2, name: 'Sara'},
-    {id: 3, name: 'Karen'},
-    {id: 4, name: 'Steve'}
-];
+ // Alert
+//  alert('Hello World');
 
-const ids = users.map(function(user){
-    return user.id;
-});
+ // Prompt
+/*  const input = prompt();
+ alert(input); */
 
-console.log(ids); */
+/*  if (confirm('Are you sure')) {
+     console.log('YES');
+ } else {
+     console.log('NO');
+ } */
 
-// FOR IN LOOP
-const user = {
-    firstName: 'John',
-    lastName: 'Connor',
-    age: 12
-}
+ let val;
 
-for(let x in user) {
-    console.log(`${x} : ${user[x]}`);
-}
+ // Outer height and width
+ val = window.outerHeight;
+ val = window.outerWidth;
+
+ // Inner height and width
+ val = window.innerHeight;
+ val = window.innerWidth;
+
+ // Scroll points
+ val = window.scrollY;
+ val = window.scrollX;
+
+ val = window.location;
+ val = window.location.hostname;
+ val = window.location.port;
+ val = window.location.href;
+ val = window.location.search;
+
+ // Redirect
+ // window.location.href = 'http://google.com';
+
+ // Reload
+ // window.location.reload();
+
+ // History Object
+
+ // window.history.go(-2);
+ val = window.history.length;
+
+ // Navigator Object
+ val = window.navigator;
+ val = window.navigator.appName;
+ val = window.navigator.appVersion;
+ val = window.navigator.userAgent;
+ val = window.navigator.platform;
+ val = window.navigator.vendor;
+ val = window.navigator.language;
+
+ console.log(val);
